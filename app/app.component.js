709b447ee25b16fa646eb058b65ff4a973da3767
Cody <meanderingmonarch@gmail.com>
2016-10-12 08:44:01 -0500

    updated some pieces for the starter kit

--- app/app.component.js
+++ app/app.component.js
@@ -11,13 +11,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 //angular component
 var core_1 = require('@angular/core');
 //decorator
+/*
+* templateurl: 'home/home.html' is how you would load a html template fyi, looks like an http request??
+*/
 var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n\t\t<div class=\"jumbotron\">\n\t\t\t<h1>Welcome to our app!</h1>\n\t\t</div>\n\t",
+            template: "\n\t\t<header>\n\t\t\t<nav class=\"navbar navbar-inverse\">\n\t\t\t\t<div class=\"navbar-header\">\n\t\t\t\t\t<a href=\"/\" class=\"navbar-brand\">My Angular 2 app</a>\n\t\t\t\t</div>\n\t\t\t</nav>\n\t\t</header>\n\t\t<div class=\"jumbotron\">\n\t\t\t<h1>Welcome to our app!</h1>\n\t\t</div>\n\n\t\t<footer class=\"text-center\">\n\t\t\tCopyright 2016\n\t\t</footer>\n\t",
             styles: ["\n\t\t.jumbotron { box-shadow: 0 2px 0 rgba(0,0,0, 0.2); }\n\t"]
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,27 @@
 //angular component
 import { Component } from '@angular/core';
 //decorator
+
+/*
+* templateurl: 'home/home.html' is how you would load a html template fyi, looks like an http request??
+*/
 @Component({
 	selector: 'my-app',
 	template: `
+		<header>
+			<nav class="navbar navbar-inverse">
+				<div class="navbar-header">
+					<a href="/" class="navbar-brand">My Angular 2 app</a>
+				</div>
+			</nav>
+		</header>
 		<div class="jumbotron">
 			<h1>Welcome to our app!</h1>
 		</div>
+
+		<footer class="text-center">
+			Copyright 2016
+		</footer>
 	`,
 	styles: [`
 		.jumbotron { box-shadow: 0 2px 0 rgba(0,0,0, 0.2); }
