16485ec0a383c22dfec28133bdbd17f98b58ef52
Cody <meanderingmonarch@gmail.com>
2016-10-12 09:38:48 -0500

    modified outDir for typescript to go into dist, deleted folders from app

--- app/app.component.ts
+++ app/app.component.ts
@@ -40,7 +40,7 @@ import { Component } from '@angular/core';
 
 //This is how you'd pass data into the component for example message = 'hello', then show in template as {{message}}
 //no need for controller.message because it's only tied into this compoenent
-
+// * is a template driven expression in *ngFor or *ngIf="users"
 export class AppComponent {
 	message = 'Hello!';
 	user = {
