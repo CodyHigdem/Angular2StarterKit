795d1d241164f70ddaba528c9b478dd19f53d798
Cody <meanderingmonarch@gmail.com>
2016-10-12 11:37:04 -0500

    refactored to use the templateurls and styleurls just found out about

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,56 +11,8 @@ import { User } from './shared/models/user';
 //DITCHES controllers and focuses on compoenents
 @Component({
 	selector: 'my-app',
-	template: `
-		<header>
-			<nav class="navbar navbar-inverse">
-				<div class="navbar-header">
-					<a href="/" class="navbar-brand">My Angular 2 app</a>
-				</div>
-			</nav>
-		</header>
-		<main>
-			<div class="row">
-				<div class="col-sm-4">
-
-					<h2> USERS ARRAY </h2>
-					<ul class="list-group users-list">
-						<li class="list-group-item"
-							*ngFor="let user of users"
-							(click)="selectUser(user)"
-							[class.active]="user === activeUser"
-						> 
-							{{user.name}} || {{user.username}} 
-
-						</li>
-					</ul>
-				</div>
-				<div class="col-sm-8">
-					<div class="jumbotron"
-						*ngIf="activeUser">
-						<h2>{{activeUser.name}} <small>{{activeUser.username}}</small></h2>
-
-						<input class="form-control" [(ngModel)]="activeUser.name">
-					</div>
-					<div class="jumbotron gocrazy" *ngIf="!activeUser">
-						<span class="glyphicon glyphicon-hand-left"></span>
-						<h2>Choose a user</h2>
-
-					</div>
-				</div>
-			</div>
-
-		</main>
-		<footer class="text-center">
-			Copyright 2016
-		</footer>
-	`,
-	styles: [`
-		.jumbotron { box-shadow: 0 2px 0 rgba(0,0,0, 0.2); }
-		.users-list li {
-			cursor: pointer;
-		}
-	`]
+	templateUrl: './app/app.component.html',
+	styleUrl: ['./app/app.component.css']
 })
 
 //[(ngModel)] 2 way databinding
